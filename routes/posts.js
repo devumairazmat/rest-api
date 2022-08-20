1877946d928b1c017105401d8a9ee7d7493be046
devumairazmat <umairazmat8260@gmail.com>
2022-08-20 20:17:48 +0500

    get and posst

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get(`/`, (req, res) => {
-  res.send("Welcome to The Posts Dev Umair Audiance");
+router.get(`/`, async(req, res) => {
+  try{
+  const post = await Post.find();
+  res.json(post);
+  }catch(err){
+    res.json({message: err});
+  }
 });
 
 router.post(`/`, async(req, res) => {
@@ -12,8 +17,13 @@ router.post(`/`, async(req, res) => {
     title: req.body.title,
     description: req.body.description,
   });
+   try{
     const savedPost = await post.save();
     res.json(savedPost);
+   }
+   catch(err) {
+    res.json({message: err});
+   }
 });
 
 module.exports = router;
