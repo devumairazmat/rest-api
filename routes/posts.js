95e570d881af1019781a8a8c191655fc2971ef58
devumairazmat <umairazmat8260@gmail.com>
2022-08-20 17:59:04 +0500

    Get and post Done

--- routes/posts.js
+++ routes/posts.js
@@ -6,22 +6,14 @@ router.get(`/`, (req, res) => {
   res.send("Welcome to The Posts Dev Umair Audiance");
 });
 
-router.post(`/`, (req, res) => {
+router.post(`/`, async(req, res) => {
   console.log(req.body);
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   });
-  post.save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json({ message : err });
-    });
-
-  //  const savedPost = await post.save();
-  //  res.json(savedPost);
+    const savedPost = await post.save();
+    res.json(savedPost);
 });
 
 module.exports = router;
