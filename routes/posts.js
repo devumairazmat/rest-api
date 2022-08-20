772bed5f60f929584656f60523da49d627f6dd2b
devumairazmat <umairazmat8260@gmail.com>
2022-08-20 22:00:55 +0500

    update done

--- routes/posts.js
+++ routes/posts.js
@@ -4,54 +4,65 @@ const Post = require("../models/Post");
 
 // Get all posts from the database
 
-router.get(`/`, async(req, res) => {
-  try{
-  const post = await Post.find();
-  res.json(post);
-  }catch(err){
-    res.json({message: err});
+router.get(`/`, async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
   }
 });
 
 // Submit The  post to the Database
 
-router.post(`/`, async(req, res) => {
+router.post(`/`, async (req, res) => {
   console.log(req.body);
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   });
-   try{
+  try {
     const savedPost = await post.save();
     res.json(savedPost);
-   }
-   catch(err) {
-    res.json({message: err});
-   }
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // get Post by ID
-router.get('/:postId' , async (req,res) => {
+router.get("/:postId", async (req, res) => {
   console.log(req.params.postId);
-  try{
-   const post = await Post.findById(req.params.postId);
-   res.json(post);
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
   }
-  catch(err) {
-    res.json({message: err});
-  }
-})
+});
 
 // Delte Post By Id
 
-router.delete('/:postId', async (req, res) => {
+router.delete("/:postId", async (req, res) => {
   try {
-    const removedPost = await Post.remove({_id: req.params.postId});
+    const removedPost = await Post.remove({ _id: req.params.postId });
     res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
   }
-  catch(err) {
-    res.json({message: err});
+});
+
+// Update the post with id
+
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatePost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatePost);
+  } catch (err) {
+    res.json({ message: err });
   }
-})
+});
 
 module.exports = router;
